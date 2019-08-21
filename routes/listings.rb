5b7377523c0e02ddf2ffa61ad6068d3003c3fe6a
billyvollman <b.vollman@gmail.com>
2019-08-22 09:58:49 +1000

    added routes

---
+++ routes/listings.rb
@@ -0,0 +1,19 @@
+get '/show' do
+    erb :show
+end
+
+get '/listings' do
+    erb :listings
+end
+
+get '/give_success' do
+    erb :give_success
+end
+
+get '/give_preview' do
+    erb :give_preview
+end
+
+get '/give_form' do
+    erb :give_form
+end
